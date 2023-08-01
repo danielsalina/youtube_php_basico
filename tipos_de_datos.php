225b2c0acda7d1fd0d7283e28f3944bf9b637bc6
danielsalina <danielorlandosalinas935@gmail.com>
2023-08-01 12:26:55 -0300

    Tipos de datos

---
+++ tipos_de_datos.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(74984); // Tipo de dato entero
+
+var_dump(587489123.54848); // Tipo de dato flotante
+
+var_dump('165edwd4823 it\'s'); // Tipo de dato string
+
+var_dump(false); // Tipos de dato booleano
+
+var_dump(array(121,1212, "hola")); // Tipo de dato array
+
+var_dump(NULL); // Tipo de dato nulo
