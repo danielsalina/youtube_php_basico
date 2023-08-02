a90cf8903c87906e9e0f6d32811d068e15982655
danielsalina <danielorlandosalinas935@gmail.com>
2023-08-02 20:07:34 -0300

    Variables

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+
+$nombre = "Dani";
+$edad = 60;
+$nombre_completo = "Daniel Salinas";
+$nombre = true;
+$edad = array("Dani", 60);
+
+// echo("Hola $nombre <br>");
+
+/* 
+print("$edad <br>");
+print_r("$nombre_completo <br>");
+var_dump("$nombre <br>");
+*/
+
+echo ($edad[0]);
+
+?>
