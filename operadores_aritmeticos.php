6938d30a10e92c3c334f872b831fe3da02f344ee
danielsalina <danielorlandosalinas935@gmail.com>
2023-08-07 18:13:40 -0300

    Operadores aritmeticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,60 @@
+<?php
+
+// Operador de suma
+$numero_1 = 10;
+$numero_2 = 8;
+
+echo $numero_1 + $numero_2 . "<br>";
+
+// Operador de resta
+$numero_1 = 10;
+$numero_2 = 8;
+
+echo $numero_1 - $numero_2 . "<br>";
+
+// Operador de multiplicación
+$numero_1 = 10;
+$numero_2 = 8;
+
+echo $numero_1 * $numero_2 . "<br>";
+
+// Operador de exponenciación
+$numero_1 = 2;
+$numero_2 = 4;
+
+echo $numero_1 ** $numero_2 . "<br>";
+
+// Operador de división
+$numero_1 = 20;
+$numero_2 = 4;
+
+echo $numero_1 / $numero_2 . "<br>";
+
+// Utilizando varios operadores aritmeticos
+// Paréntesis, Exponentes, Multiplicación y División (de izquierda a derecha), Suma y Resta (de izquierda a derecha)
+$numero_1 = 2;
+$numero_2 = 4;
+$numero_3 = 1;
+$numero_4 = 5;
+$numero_5 = 2;
+$numero_6 = 4;
+
+echo ($numero_6 + $numero_2 - $numero_1) / $numero_3 * $numero_5 ** $numero_4 . "<br>";
+
+// Operador de aumento
+$numero_1 = 20;
+
+echo $numero_1++ . "<br>";
+echo $numero_1 . "<br>";
+
+// Operador de decremento
+$numero_1 = 20;
+
+echo $numero_1-- . "<br>";
+echo $numero_1 . "<br>";
+
+// Operador de módulo
+$numero_1 = 8;
+$numero_2 = 2;
+
+echo $numero_1 % $numero_2 . "<br>";
