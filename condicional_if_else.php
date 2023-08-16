67cb6f02709d103c2b10b5b0663d4ae88007f1c3
danielsalina <danielorlandosalinas935@gmail.com>
2023-08-16 18:19:57 -0300

    Condicional if else

---
+++ condicional_if_else.php
@@ -0,0 +1,25 @@
+<?php
+
+$edad = 19;
+
+if($edad >= 18 AND $edad <= 30){
+    // echo "¡Bienvenido al club!";
+}else {
+    // echo "Te pasas de la edad 😔";
+}
+
+$gym = false;
+
+if($gym){
+    // echo "Vamos a entrenar 💪🏼";
+}else {
+    // echo "Nos quedamos surmiendo 😞";
+}
+
+$english = false;
+
+if ($english) {
+    echo "Vamos por ello 🤙🏼";
+} else {
+    echo "No cumplimos con el objetivo 😞";
+}
