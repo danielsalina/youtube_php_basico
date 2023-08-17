fd26d234eb1da854892ce02b9a16ea8b4d14a8b9
danielsalina <danielorlandosalinas935@gmail.com>
2023-08-17 17:37:52 -0300

    Operador ternario

---
+++ operador_ternario.php
@@ -0,0 +1,14 @@
+<?php
+
+// Sintaxis 
+// condicion ? "condición verdadera" : "condición falsa"
+
+$edad = 99;
+
+// echo ($edad >= 18 AND $edad <= 30) ? "¡Bienvenido al club!" : "Te pasas de la edad 😔";
+
+$gym = true;
+
+// echo $gym ? "Vamos a entrenar 💪🏼" : "Nos quedamos durmiendo 😞";
+
+echo (true) ? "Vamos por ello 🤙🏼" . 1 + 9 . "pts!" : "No cumplimos con el objetivo 😞";
