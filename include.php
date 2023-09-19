1571ee4940c969dca6c22bc67284c9f390d4c01a
danielsalina <danielorlandosalinas935@gmail.com>
2023-09-19 18:33:48 -0300

    Función include

---
+++ include.php
@@ -0,0 +1,17 @@
+<?php
+
+include("argumentos_nombrados.php");
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+include("array.php");
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+include("argumentos_nombrados.php");
+
+echo "Sigo con mi vida..";
