c7418f5d25403005128c3b63a38d67718f991108
danielsalina <danielorlandosalinas935@gmail.com>
2023-09-20 17:49:46 -0300

    Función include once

---
+++ include_once.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once("argumentos_nombrados.php");
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+include_once("array.php");
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+include_once("argumentos_nombrados.php");
+
+echo "Sigo con mi vida......";
+
+include_once("archivo_inexistente.php");
+
+echo "Sigo con mi vida 2......";
+
+// obtenerSaludoAleatorio();
+
+// echo "Sigo con mi vida 3......";
